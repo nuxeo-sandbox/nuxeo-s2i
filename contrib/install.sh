55db672a5d8827a0640a505687f75ed5cfa202c8
Damien Metzler <dmetzler@nuxeo.com>
2017-10-05 13:45:08 -0700

    wip

--- contrib/install.sh
+++ contrib/install.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 
+# This script install all builded artifact into 
+# a Nuxeo based image :
+#   * JAR artifacts in `artifacts` are copied into the Nuxeo `bundles` directory
+#   * Nuxeo Package in `marketplace` is installed thru mp-install
+#   * `nuxeo.conf` file is appended to regular nuxeo.conf
+#
+
+
 if [ -d /build/artifacts ]; then
-	cp /build/artifacts/*.jar $NUXEO_HOME/nxserver/bundles
+	echo "---> Copying JAR artifacts in bundles directory"  
+	cp -v /build/artifacts/*.jar $NUXEO_HOME/nxserver/bundles
 fi
 
 
 if [ -f /build/nuxeo.conf ]; then
-	cp /build/nuxeo.conf /docker-entrypoint-init.d/
+	echo "---> Copying nuxeo.conf"  
+	cp -v /build/nuxeo.conf /docker-entrypoint-init.d/
+fi
+
+if [ -f /connect.properties ]; then
+  echo "---> Found connect.properties file"  
+  . /connect.properties
+
+  if [ -n "$NUXEO_CONNECT_USERNAME" -a -n "$NUXEO_CONNECT_PASSWORD" -a -n "$NUXEO_STUDIO_PROJECT" ]; then  
+    echo "---> Configuring connect credentials"
+    /docker-entrypoint.sh nuxeoctl register $NUXEO_CONNECT_USERNAME $NUXEO_STUDIO_PROJECT dev openshift $NUXEO_CONNECT_PASSWORD
+  fi
+
+else 
+  echo "---> No connect.properties found"
 fi
 
 if [ -d /build/marketplace ]; then
-  echo "---> Installing Nuxeo Package for project from $LOCAL_SOURCE_DIR/$NUXEO_PACKAGE"  
-  PACKAGE=$(/build/marketplace)
-  echo "---> Found package $PACKAGE"
-  /docker-entrypoint.sh nuxeoctl mp-init
-  /docker-entrypoint.sh $NUXEO_HOME/bin/nuxeoctl mp-install $PACKAGE
+
+  
+  PACKAGE=$(ls /build/marketplace)
+  if [ -n $PACKAGE ]; then
+	  echo "---> Found package $PACKAGE"
+	  echo "---> Installing Nuxeo Package for project from $LOCAL_SOURCE_DIR/$NUXEO_PACKAGE"  
+	  /docker-entrypoint.sh nuxeoctl mp-init
+	  /docker-entrypoint.sh $NUXEO_HOME/bin/nuxeoctl mp-install $PACKAGE
+  else
+  	  echo "---> No Nuxeo Package found"
+  fi
 fi
